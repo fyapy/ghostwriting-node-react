2884d4c56c509690787e39eb4cd00653478fa8d5
Amir <fyapy2@mail.ru>
2018-12-28 03:36:11 +0300

    Added error messaging to authentication pages

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import classnames from "classnames";
 
 class Register extends Component {
   constructor() {
@@ -29,45 +31,72 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    console.log(newUser);
+    axios
+      .post("/api/users/register", newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
       <div>
         <h1>Register</h1>
-        <form onSubmit={this.onSubmit}>
+        <form noValidate onSubmit={this.onSubmit}>
           <input
             type="text"
             name="name"
+            className={classnames("form-control", {
+              "is-invalid": errors.name
+            })}
             value={this.state.name}
             placeholder="Name"
             onChange={this.onChange}
           />
+          {errors.name && <div className="invalid-feedback">{errors.name}</div>}
           <br />
           <input
             type="email"
             name="email"
+            className={classnames("form-control", {
+              "is-invalid": errors.email
+            })}
             value={this.state.email}
             placeholder="Email"
             onChange={this.onChange}
           />
+          {errors.email && (
+            <div className="invalid-feedback">{errors.email}</div>
+          )}
           <br />
           <input
             type="text"
             name="password"
+            className={classnames("form-control", {
+              "is-invalid": errors.password
+            })}
             value={this.state.password}
             placeholder="Password"
             onChange={this.onChange}
           />
+          {errors.password && (
+            <div className="invalid-feedback">{errors.password}</div>
+          )}
           <br />
           <input
             type="text"
             name="password2"
+            className={classnames("form-control", {
+              "is-invalid": errors.password2
+            })}
             value={this.state.password2}
             placeholder="Confirm Password"
             onChange={this.onChange}
           />
+          {errors.password2 && (
+            <div className="invalid-feedback">{errors.password2}</div>
+          )}
           <br />
           <input type="submit" value="submit" />
         </form>
